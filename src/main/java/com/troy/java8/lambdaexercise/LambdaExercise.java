20b913346f82b9e57a38e511c7e82c298d7860ce
wowmama <wowmama0107@gmail.com>
2017-03-01 21:05:34 +0800

    LambdaExercise-1. Create exercise template.

---
+++ src/main/java/com/troy/java8/lambdaexercise/LambdaExercise.java
@@ -0,0 +1,29 @@
+package com.troy.java8.lambdaexercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaExercise {
+
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(
+					new Person("Charles", "Dickens", 60),
+					new Person("Lewis", "Carroll", 42),
+					new Person("Thomas", "Carlyle", 51),
+					new Person("Charlatte", "Bronte", 45),
+					new Person("Matthew", "Arrold", 39)
+				);
+		
+		//用lastName分類
+		
+		
+		//寫個function因出所有人的資訊
+		
+		
+		//寫個方法印出lastName開頭是C的人
+
+	}
+
+}
